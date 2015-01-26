a8f5a69a92b38e37aac92155fddaeb28c0a359ed
prakash kumar <prakash4u05@gmail.com>
2015-01-26 16:01:07 +0530

    'sorting'

---
+++ max.java
@@ -0,0 +1,12 @@
+class Max {
+	public static void main(String[] args) {
+		int[] a = {1,3,2,4,5};
+
+		mysort(a);
+	}
+
+	// my selection sort
+	public static void mysort(int[] a) {
+		System.out.println("mysort");		
+	}
+}
